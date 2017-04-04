ab92b0d91ceec7437d2bc4489fa9430b22547416
Jack Preston <unwttng@gmail.com>
2017-04-04 15:01:10 +0100

    Queen attacking player

--- sample.js
+++ sample.js
@@ -1,8 +1,8 @@
 const { Game } = require('./lib/Game')
-const { RandomBotPlayer } = require('./lib/Player')
+const { RandomBotPlayer, RandomQueenAttackingBotPlayer } = require('./lib/Player')
 
 const white = new RandomBotPlayer({ logFn: console.log })
-const black = new RandomBotPlayer({ logFn: console.log })
+const black = new RandomQueenAttackingBotPlayer({ logFn: console.log })
 
 const game = new Game(white, black, { logFn: console.log })
 game.begin()
