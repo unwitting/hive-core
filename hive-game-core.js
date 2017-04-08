9826bc14d74e2e78f5e47bdc29c9d2676c6df170
Jack Preston <unwttng@gmail.com>
2017-04-08 12:08:04 +0100

    Exporting move

--- hive-game-core.js
+++ hive-game-core.js
@@ -1,5 +1,6 @@
 const { Board } = require('./lib/Board')
 const { Game } = require('./lib/Game')
 const { Player } = require('./lib/Player')
+const { Move } = require('./lib/Move')
 
-module.exports = { Board, Game, Player }
+module.exports = { Board, Game, Player, Move }
